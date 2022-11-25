0fc756f061617caafab37c53cbe21948032e4f79
Bruce Parrello <bparrello@figresearch.com>
2022-11-25 12:54:32 -0600

    Refactored Shuffler.

--- src/main/java/org/theseed/dl4j/train/LearningProcessor.java
+++ src/main/java/org/theseed/dl4j/train/LearningProcessor.java
@@ -20,8 +20,8 @@ import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.theseed.counters.Shuffler;
 import org.theseed.dl4j.train.Trainer.Type;
-import org.theseed.io.Shuffler;
 import org.theseed.reports.IValidationReport;
 
 public class LearningProcessor extends ModelProcessor {

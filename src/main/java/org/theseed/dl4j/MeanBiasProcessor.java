c7acfb885b64900b4a6d261af66cd68ba67823d7
Bruce Parrello <bparrello@figresearch.com>
2024-04-17 13:26:13 -0500

    Refactoring for BaseReportProcessor.

--- src/main/java/org/theseed/dl4j/MeanBiasProcessor.java
+++ src/main/java/org/theseed/dl4j/MeanBiasProcessor.java
@@ -24,10 +24,10 @@ import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.Option;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.theseed.basic.BaseReportProcessor;
 import org.theseed.basic.ParseFailureException;
 import org.theseed.io.LineReader;
 import org.theseed.io.TabbedLineReader;
-import org.theseed.utils.BaseReportProcessor;
 import org.theseed.utils.Parms;
 
 /**
